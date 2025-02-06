74784d460f804c884966e41c4c4d8566389f9afc
SaveKit <kittipit582@gmail.com>
2025-02-06 22:50:24 +0700

    add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <span style={{ color: 'black' }}>KnowYour</span>
+          <span style={{ color: 'var(--primary-blue-color)' }}>Bones</span>
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/learn">เรียนรู้</Link></li>
+        <li><Link to="/exercises">แบบฝึกหัด</Link></li>
+        <li><Link to="/profile">โปรไฟล์</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
