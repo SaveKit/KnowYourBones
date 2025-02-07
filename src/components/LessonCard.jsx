5fa5c5e8c491fd88cdfa7b3f56fb8cc1e2eaa652
SaveKit <kittipit582@gmail.com>
2025-02-07 15:10:21 +0700

    created component lesson card

---
+++ src/components/LessonCard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import "./LessonCard.css";
+
+const LessonCard = ({ title, image, link }) => {
+  return (
+    <Link to={link} className="learn-more">
+      <div className="lesson-card">
+        <div className="lesson-content">
+          <h2>{title}</h2>
+          <Link to={link} className="learn-more">
+            ➜ เรียนรู้เพิ่มเติม
+          </Link>
+        </div>
+        <img src={image} alt={title} className="lesson-image" />
+      </div>
+    </Link>
+  );
+};
+
+LessonCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
+
+export default LessonCard;
